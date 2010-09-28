48d0fd96aaec9dc156523be26148c4e8f261cd39
David Vazgenovich Shakaryan <dvshakaryan@gmail.com>
2010-09-28 01:40:21 -0700

    Add skeleton Ruby script to replace shell script.

---
+++ lognotify.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+#
+# lognotify.rb
+# http://github.com/omp/lognotify
+#
+# Copyright 2010 David Vazgenovich Shakaryan <dvshakaryan@gmail.com>
+# Distributed under the terms of the GNU General Public License v3.
+# See http://www.gnu.org/licenses/gpl.txt for the full license text.
